45c271fc8d0f2815683a4e48e6c0dfc551d0f975
Trial97 <alexandru.tripon97@gmail.com>
2023-06-28 01:44:49 +0300

    6

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/printexperiment">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
