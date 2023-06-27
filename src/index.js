1ef9dea27391827602efd5d5052a240f309c895c
Trial97 <alexandru.tripon97@gmail.com>
2023-06-28 01:38:20 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  HashRouter,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/printexperiment",
-    element: <App />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/printexperiment">
-      <RouterProvider router={router} />
-    </HashRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
